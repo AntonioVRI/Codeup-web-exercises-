c998fba189252c28460a28316b45b191161b7a00
AntonioVRI <avidaurri4@gmail.com>
2020-09-28 09:34:27 -0500

    warmup files

---
+++ js/warmup.js
@@ -0,0 +1,12 @@
+console.log("Anthony");
+console.log("Susan");
+console.log("Rose");
+console.log("Maggie");
+console.log("Jess");
+
+
+function sayHello(name) {
+    console.log("Hello " + name);
+    }
+    sayHello(name: "Anthony");
+    sayHello(name: "Rose");
