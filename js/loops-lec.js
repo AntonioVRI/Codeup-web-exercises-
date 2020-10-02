4f9c23610734fbf4a0bb65f20b567338653a98e7
AntonioVRI <avidaurri4@gmail.com>
2020-10-02 14:41:18 -0500

    initial commit

--- js/loops-lec.js
+++ js/loops-lec.js
@@ -82,12 +82,12 @@
 // } while (condition)
 
 
-var i = 10;
-
-while (i < 10) {
-	console.log('while loop iteration #' + i);
-	i++;
-}
+// var i = 10;
+//
+// while (i < 10) {
+// 	console.log('while loop iteration #' + i);
+// 	i++;
+// }
 
 
 // TODO TOGETHER: Why doesnt the example above run?
@@ -139,13 +139,25 @@ while (i < 10) {
 
 // TODO TOGETHER: Write a for loop that loops through numbers 0 - 5 (inclusive);
 
+// for(var i = 0; i <= 5; i++){
+// 	console.log(i);
+// }
 
 // TODO TOGETHER: Why are loops beneficial?
+//To run code in a program repeatedly without having to be redundant in your writing
 
 
 // TODO TOGETHER: Console.log each color from the array randomColors
-//  var randomColors = ["red","orange","yellow","blue","green"]
+//for(initialize, condition, increment)
 
+//   var randomColors = ["red","orange","yellow","blue","green"]
+// console.log(randomColors.indexOf)
+
+
+
+// for(var index = 0; index < 5; index++){
+// 	console.log(randomColors[index]);
+// }
 
 // for (var i = 0; i < randomColors.length; i++){
 // 	console.log(randomColors[i]);;
@@ -155,9 +167,20 @@ while (i < 10) {
 
 // TODO TOGETHER: Using a for loop, log all even numbers 0 - 100;
 
+// for(var i = 0; i <= 100; i++){
+// 		if(i % 2 === 0){
+// 			console.log(i);
+// 		}
+// }
+
 
 // TODO: Using a for loop, log all numbers divisible by 5, from 0 - 100;
 
+// for(var i = 0; i <= 100; i++){
+// 	if(i % 5 === 0){
+// 		console.log(i);
+// 	}
+// }
 
 // TODO: Using a for loop, complete the FizzBuzz challenge:
 //  Your program should count up to 100.
@@ -166,6 +189,18 @@ while (i < 10) {
 //  If divisible by 3 and 5, log "FizzBuzz"
 //  All other cases, just log the number.
 
+// for(var i = 0; i <= 100; i++){
+// 	if((i % 3 === 0) && (i % 5 === 0)){
+// 		console.log("Fizz");
+// 	}else if(i % 3 === 0){
+// 		console.log("Buzz")
+// 	}else if(i % 5 === 0){
+// 		console.log("FizzBuzz");
+// 	}else{
+// 		console.log(i);
+// 	}
+//
+// }
 
 
 /*********************************************
