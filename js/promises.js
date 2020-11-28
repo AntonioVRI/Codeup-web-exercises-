36e31b889f6bb99647dae8606d286b1bb3da6c2e
Anthony V <avidaurri4@gmail.com>
2020-11-27 23:52:31 -0600

    restful-api example

--- js/promises.js
+++ js/promises.js
@@ -23,3 +23,24 @@ function wait(numMilliseconds) {
 
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+function extractDateOfLastPushEvent(events) {
+    for (let event of events) {
+        if (event.type === 'PushEvent') {
+            return new Date(event.created.at).toDateString();
+        }
+    }
+    return null;
+}
+
+function lastPushDate(username) {
+    const url = 'https://api.github.com/users/${username}/events';
+    const options = {headers: {'Authorization': 'token ' + gitHubToken}}
+    return fetch(url, options)
+        .then(res => res.json())
+        .then(extractDateOfLastPushEvent)
+        .catch(console.error);
+}
+
+lastPushDate("").then(console.log);
