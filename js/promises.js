e455f66278890a6f90996da38d90037a91a0f3cf
Anthony V <avidaurri4@gmail.com>
2020-12-01 17:05:18 -0600

    12/1 exercises

--- js/promises.js
+++ js/promises.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 function wait(numMilliseconds) {
     return new Promise((resolve, reject) => {
        setTimeout(() => {
@@ -16,10 +15,6 @@ function wait(numMilliseconds) {
 //         setTimeout(resolve, numMilliseconds)
 //     });
 // }
-
-
-
-
 
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
